6083f560d69c2913616a0d5ae1d2882f6228554d
adrxnl <162568872+adrxnl@users.noreply.github.com>
2025-10-07 02:30:12 -0500

    Update user.model.js with role-specific fields
    Creates fields that are relevant for each role, varying in structure for the sake of future development. More fields to be developed.

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -13,4 +13,19 @@ const userSchema = new mongoose.Schema({
         required: true,
         enum: ['athlete', 'coach', 'lawyer'] // TBD: admin, company
     },
+
+    // -- Role-specific fields --
+    // Athlete:
+    schoolEmail: { type: String },
+    stats: { type: Map, of: String },
+    highlights: [{ type: String }], // Array of references to highlight media
+
+    // Coach:
+    team: { type: String },
+    experience: [{ type: String }],
+
+    // Lawyer:
+    firm: { type: String },
+
 }, { timestamps: true }); // Automatically adds createdAt and updatedAt fields
+
