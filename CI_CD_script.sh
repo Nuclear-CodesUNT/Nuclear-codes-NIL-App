4ce8b77eb03536597c171bf24a7a332cc9649e51
Micheal Landsbaum <micheallandsbaum@my.unt.edu>
2025-10-16 15:08:08 -0500

    Update CI_CD_script.sh

--- CI_CD_script.sh
+++ CI_CD_script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 sudo git fetch
-echo mdl0245
-echo $1
+exec echo mdl0245
+exec echo $1
 sudo git pull
-echo mdl0245
-echo $1
+exec echo mdl0245
+exec echo $1
 sudo systemctl restart NIL_app_frontend.service && sudo systemctl restart NIL_app_backend.service
