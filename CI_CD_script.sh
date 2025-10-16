107afd5a4967289d810a98b4f29aceeff4f16cb9
Micheal Landsbaum <micheallandsbaum@my.unt.edu>
2025-10-16 15:18:07 -0500

    Update CI_CD_script.sh

--- CI_CD_script.sh
+++ CI_CD_script.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-sudo git pull https://mdl0245:$1/Nuclear-CodesUNT/Nuclear-codes-NIL-App.git
+sudo git fetch https://mdl0245:$1/Nuclear-CodesUNT/Nuclear-codes-NIL-App.git mdl0245-CI
+sudo git pull https://mdl0245:$1/Nuclear-CodesUNT/Nuclear-codes-NIL-App.git mdl0245-CI
 sudo systemctl restart NIL_app_frontend.service && sudo systemctl restart NIL_app_backend.service
